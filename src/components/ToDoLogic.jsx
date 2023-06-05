a3c506bfde7511fbdd33187efd9edbf75b0f10be
rachaouldbabaali <rachaould@gmail.com>
2023-06-05 20:34:18 +0100

    Add new task

--- src/components/ToDoLogic.jsx
+++ src/components/ToDoLogic.jsx
@@ -1,23 +1,14 @@
 import InputTodo from "./InputToDo";
 import TodosList from "./TodoList";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const TodosLogic = () => {
   const [todos, setTodos] = useState([
     {
-      id: 1,
+      id: uuidv4(),
       title: "Setup development environment",
       completed: true,
-    },
-    {
-      id: 2,
-      title: "Develop website and add content",
-      completed: false,
-    },
-    {
-      id: 3,
-      title: "Deploy to live server",
-      completed: false,
     },
   ]);
   const handleChange = (id) => {
@@ -41,9 +32,18 @@ const TodosLogic = () => {
     ]);
   };
 
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
   return (
     <div>
-      <InputTodo />
+      <InputTodo addTodoItem={addTodoItem} />
       <TodosList
         todosProps={todos}
         handleChange={handleChange}
