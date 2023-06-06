7b01c257da6b3e2842d48e8bb7b7ca703b5dd06a
rachaouldbabaali <rachaould@gmail.com>
2023-06-06 15:07:15 +0100

    Style delete button

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import ToDoApp from "./components/ToDoApp";
-import "./App.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ToDoApp from './components/ToDoApp';
+import './App.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
